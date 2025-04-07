609893b9609992a8ca0d4fc0b768569132cb0190
MilitsaTsvetkova <militsa88@yahoo.com>
2025-04-07 10:17:13 +0300

    flex alignment

--- App.js
+++ App.js
@@ -6,21 +6,24 @@ export default function App() {
       style={{
         backgroundColor: "white",
         flex: 1,
-        flexDirection: "row-reverse",
+        flexDirection: "row", //horizontal axis // column - vertical axis
+        justifyContent: "center", //main axis
+        alignItems: "center", //secondary axis
       }}
     >
       <View
         style={{
           backgroundColor: "blue",
           width: 100,
-          height: 100,
+          height: 300,
+          alignSelf: "flex-start",
         }}
       />
       <View
         style={{
           backgroundColor: "orange",
           width: 100,
-          height: 100,
+          height: 200,
         }}
       />
       <View

3a4c0af5d026a45594c255b92eff945b8e6067c6
MilitsaTsvetkova <militsa88@yahoo.com>
2025-04-07 09:47:16 +0300

    initial setup

--- App.js
+++ App.js
@@ -1,20 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  Button,
+  Platform,
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+} from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      {/* <Text>Hello App!</Text>
+      <Image
+        //  source={require('./assets/icon.png')}
+        source={{
+          uri: "https://example.com/icon.png",
+          width: 200,
+          height: 300,
+        }}
+      />
+      */}
+
+      <Button
+        title="Click Me"
+        // onPress={() =>
+        //   Alert.alert({
+        //     title: "Button Pressed",
+        //     message: "You clicked the button!",
+        //     buttons: [
+        //       {
+        //         text: "OK",
+        //         onPress: () => console.log("OK Pressed"),
+        //       },
+        //       {
+        //         text: "Cancel",
+        //         onPress: () => console.log("Cancel Pressed"),
+        //       },
+        //     ],
+        //   })
+        // }
+        onPress={() =>
+          Alert.prompt("Button Pressed", "You clicked the button!", (text) =>
+            console.log(text)
+          )
+        }
+      />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    // alignItems: "center",
+    // justifyContent: "center",
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
   },
 });

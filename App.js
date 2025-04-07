bea5e9fe137d5e8553042960ad8703c42c3a0b5a
MilitsaTsvetkova <militsa88@yahoo.com>
2025-04-07 10:28:54 +0300

    flex wrapping

--- App.js
+++ App.js
@@ -8,27 +8,42 @@ export default function App() {
         flex: 1,
         flexDirection: "row", //horizontal axis // column - vertical axis
         justifyContent: "center", //main axis
-        alignItems: "center", //secondary axis
+        alignItems: "center", //secondary axis on each line
+        alignContent: "center", //align the content in the container
+        flexWrap: "wrap", //if the screen is not enough, it will wrap to the next line
       }}
     >
       <View
         style={{
           backgroundColor: "blue",
           width: 100,
-          height: 300,
-          alignSelf: "flex-start",
+          height: 100,
         }}
       />
       <View
         style={{
           backgroundColor: "orange",
           width: 100,
-          height: 200,
+          height: 100,
         }}
       />
       <View
         style={{
           backgroundColor: "tomato",
+          width: 100,
+          height: 100,
+        }}
+      />
+      <View
+        style={{
+          backgroundColor: "grey",
+          width: 100,
+          height: 100,
+        }}
+      />
+      <View
+        style={{
+          backgroundColor: "greenyellow",
           width: 100,
           height: 100,
         }}

b151e8f96be7e24360eb18fcf479c95f8921268b
MilitsaTsvetkova <militsa88@yahoo.com>
2025-04-08 20:24:33 +0300

    added logo

--- App.js
+++ App.js
@@ -1,20 +1,5 @@
-import { View } from "react-native";
 import WelcomeScreen from "./app/screens/WelcomeScreen.js";
 
 export default function App() {
-  return (
-    <View
-      style={{
-        backgroundColor: "white",
-        flex: 1,
-        flexDirection: "row", //horizontal axis // column - vertical axis
-        justifyContent: "center", //main axis
-        alignItems: "center", //secondary axis on each line
-        // alignContent: "center", //align the content in the container
-        // flexWrap: "wrap", //if the screen is not enough, it will wrap to the next line
-      }}
-    >
-      <WelcomeScreen />
-    </View>
-  );
+  return <WelcomeScreen />;
 }
--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,12 +1,20 @@
-import { ImageBackground, StyleSheet } from "react-native";
+import { Image, ImageBackground, StyleSheet, Text, View } from "react-native";
 
 function WelcomeScreen() {
   return (
     <ImageBackground
-      source={require("../assets/adaptive-icon.png")}
+      source={require("../assets/background.jpg")}
       style={styles.background}
     >
-      <View style={styles.loginButton}></View>
+      <View style={styles.logoContainer}>
+        <Image
+          source={require("../assets/logo-red.png")}
+          styles={styles.logo}
+        />
+        <Text>Sell What You Don't Need</Text>
+      </View>
+      <View style={styles.loginButton} />
+      <View style={styles.registerButton} />
     </ImageBackground>
   );
 }
@@ -17,10 +25,25 @@ const styles = StyleSheet.create({
   background: {
     flex: 1,
     justifyContent: "flex-end",
+    alignItems: "center",
   },
   loginButton: {
     width: "100%",
     height: 70,
     backgroundColor: "#fc5c65",
+  },
+  registerButton: {
+    width: "100%",
+    height: 70,
+    backgroundColor: "#4ecdc4",
+  },
+  logo: {
+    width: 100,
+    height: 100,
+  },
+  logoContainer: {
+    position: "absolute",
+    top: 70,
+    alignItems: "center",
   },
 });

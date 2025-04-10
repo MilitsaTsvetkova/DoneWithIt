eec970447017603f364640b471d76a7e74080c1f
MilitsaTsvetkova <militsa88@yahoo.com>
2025-04-10 09:59:21 +0300

    replaced icon placeholders with MaterialCommunityIcons

--- app/screens/ViewImageScreen.js
+++ app/screens/ViewImageScreen.js
@@ -1,11 +1,20 @@
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { Image, StyleSheet, View } from "react-native";
 import colors from "../config/colors";
 
 function ViewImageScreen() {
   return (
     <View style={styles.container}>
-      <View style={styles.closeIcon}></View>
-      <View style={styles.deleteIcon}></View>
+      <View style={styles.closeIcon}>
+        <MaterialCommunityIcons name="close" size={35} color={colors.white} />
+      </View>
+      <View style={styles.deleteIcon}>
+        <MaterialCommunityIcons
+          name="trash-can-outline"
+          size={35}
+          color={colors.white}
+        />
+      </View>
       <Image
         resizeMode="contain"
         style={styles.image}
@@ -23,17 +32,11 @@ const styles = StyleSheet.create({
     backgroundColor: colors.black,
   },
   closeIcon: {
-    width: 50,
-    height: 50,
-    backgroundColor: colors.primary,
     position: "absolute",
     top: 40,
     left: 30,
   },
   deleteIcon: {
-    width: 50,
-    height: 50,
-    backgroundColor: colors.secondary,
     position: "absolute",
     top: 40,
     right: 30,

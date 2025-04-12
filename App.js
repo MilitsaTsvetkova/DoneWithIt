ef9a286ec199aac69958ca474f84e35c75db4f1d
MilitsaTsvetkova <militsa88@yahoo.com>
2025-04-12 14:32:04 +0300

    AppPicker component initial setup

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import { GestureHandlerRootView } from "react-native-gesture-handler";
+import AppPicker from "./app/components/AppPicker";
 import AppTextInput from "./app/components/AppTextInput";
 import Screen from "./app/components/Screen";
 
@@ -6,7 +7,8 @@ export default function App() {
   return (
     <GestureHandlerRootView>
       <Screen>
-        <AppTextInput placeholder="Email" icon="email" />
+        <AppPicker placeholder="Category" icon="apps" />
+        <AppTextInput icon="email" placeholder="Email" />
       </Screen>
     </GestureHandlerRootView>
   );

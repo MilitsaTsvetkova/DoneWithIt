1d62f66e146d6ca55871e94a2062933c13c4d8be
MilitsaTsvetkova <militsa88@yahoo.com>
2025-04-14 16:42:42 +0300

    Truncate long text on AppText

--- app/components/Card.js
+++ app/components/Card.js
@@ -8,8 +8,12 @@ function Card({ image, title, subTitle }) {
       <Image source={image} style={styles.image} />
 
       <View style={styles.detailsContainer}>
-        <AppText style={styles.title}>{title}</AppText>
-        <AppText style={styles.subtitle}>{subTitle}</AppText>
+        <AppText style={styles.title} numberOfLines={1}>
+          {title}
+        </AppText>
+        <AppText style={styles.subtitle} numberOfLines={2}>
+          {subTitle}
+        </AppText>
       </View>
     </View>
   );

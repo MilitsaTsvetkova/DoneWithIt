c32eec305e7834ad03581a6e5c4f0bf57c9545f0
MilitsaTsvetkova <militsa88@yahoo.com>
2025-04-14 16:01:48 +0300

    style AppPicker placeholder

--- app/components/AppPicker.js
+++ app/components/AppPicker.js
@@ -37,7 +37,11 @@ function AppPicker({ placeholder, icon, items, selectedItem, onItemSelect }) {
               color={defaultStyles.colors.medium}
             />
           )}
-          <AppText style={styles.text}>{selectedItem ?? placeholder}</AppText>
+          {selectedItem ? (
+            <AppText style={styles.text}>{selectedItem}</AppText>
+          ) : (
+            <AppText style={styles.placeholder}>{placeholder}</AppText>
+          )}
           <MaterialCommunityIcons
             name="chevron-down"
             size={20}
@@ -76,6 +80,10 @@ const styles = StyleSheet.create({
   },
   icon: {
     marginRight: 10,
+  },
+  placeholder: {
+    color: defaultStyles.colors.medium,
+    flex: 1,
   },
   text: {
     flex: 1,

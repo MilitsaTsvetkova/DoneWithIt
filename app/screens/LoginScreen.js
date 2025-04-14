77253e74eec1ac38120637e964855224d97e32d7
MilitsaTsvetkova <militsa88@yahoo.com>
2025-04-14 09:51:13 +0300

    built the form component

--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -1,6 +1,6 @@
-import { Formik } from "formik";
 import { Image, StyleSheet } from "react-native";
 import Screen from "../components/Screen";
+import AppForm from "../components/forms/AppForm";
 import AppFormField from "../components/forms/AppFormField";
 import SubmitButton from "../components/forms/SubmitButton";
 
@@ -19,35 +19,31 @@ function LoginScreen() {
         style={styles.logo}
         resizeMode="cover"
       />
-      <Formik
+      <AppForm
         initialValues={{ email: "", password: "" }}
         onSubmit={(values) => console.log(values)}
         validationSchema={validationSchema}
       >
-        {() => (
-          <>
-            <AppFormField
-              name="email"
-              icon="email"
-              autoCapitalize="none"
-              autoCorrect={false}
-              placeholder="Email"
-              keyboardType="email-address"
-              textContentType="emailAddress"
-            />
-            <AppFormField
-              name="password"
-              autoCapitalize="none"
-              autoCorrect={false}
-              icon="lock"
-              placeholder="Password"
-              secureTextEntry
-              textContentType="password"
-            />
-            <SubmitButton title="Login" />
-          </>
-        )}
-      </Formik>
+        <AppFormField
+          name="email"
+          icon="email"
+          autoCapitalize="none"
+          autoCorrect={false}
+          placeholder="Email"
+          keyboardType="email-address"
+          textContentType="emailAddress"
+        />
+        <AppFormField
+          name="password"
+          autoCapitalize="none"
+          autoCorrect={false}
+          icon="lock"
+          placeholder="Password"
+          secureTextEntry
+          textContentType="password"
+        />
+        <SubmitButton title="Login" />
+      </AppForm>
     </Screen>
   );
 }

bf247e72cbaee44a89ceacc0be5cd1ea958b350f
MilitsaTsvetkova <militsa88@yahoo.com>
2025-04-14 15:13:31 +0300

    RegisterScreen

---
+++ app/screens/RegisterScreen.js
@@ -0,0 +1,57 @@
+import { StyleSheet } from "react-native";
+import * as Yup from "yup";
+import Screen from "../components/Screen";
+import { AppForm, AppFormField, SubmitButton } from "../components/forms";
+
+const validationSchema = Yup.object().shape({
+  name: Yup.string().required().label("Name"),
+  email: Yup.string().email().required().label("Email"),
+  password: Yup.string().required().min(4).label("Password"),
+});
+
+function RegisterScreen(props) {
+  return (
+    <Screen>
+      <AppForm
+        initialValues={{ name: "", email: "", password: "" }}
+        onSubmit={(values) => console.log(values)}
+        validationSchema={validationSchema}
+      >
+        <AppFormField
+          name="name"
+          icon="account"
+          placeholder="Name"
+          autoCapitalize="none"
+          autoCorrect={false}
+        />
+        <AppFormField
+          name="email"
+          icon="email"
+          placeholder="Email"
+          autoCapitalize="none"
+          autoCorrect={false}
+          keyboardType="email-address"
+          textContentType="emailAddress"
+        />
+        <AppFormField
+          name="password"
+          icon="lock"
+          placeholder="Password"
+          autoCapitalize="none"
+          autoCorrect={false}
+          secureTextEntry
+          textContentType="password"
+        />
+        <SubmitButton title="Register" />
+      </AppForm>
+    </Screen>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 10,
+  },
+});
+
+export default RegisterScreen;

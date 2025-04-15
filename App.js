99658cdb4af025fbab24e6c4dd95bcd5a7e1a637
MilitsaTsvetkova <militsa88@yahoo.com>
2025-04-15 10:14:50 +0300

    ImageInput

--- App.js
+++ App.js
@@ -1,28 +1,15 @@
-import * as ImagePicker from "expo-image-picker";
-import { useEffect } from "react";
-import { Text } from "react-native";
+import { useState } from "react";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
+import ImageInput from "./app/components/ImageInput";
 import Screen from "./app/components/Screen";
 
 export default function App() {
-  const requestPermissions = async () => {
-    //alternative - can use this to request multiple permissions
-    // const { granted } = await Permissions.askAsync(
-    //   Permissions.MEDIA_LIBRARY,
-    //   Permissions.CAMERA
-    // );
-    const { granted } = await ImagePicker.requestCameraPermissionsAsync();
-    if (!granted) {
-      alert("Sorry, we need camera roll permissions to make this work!");
-    }
-  };
-  useEffect(() => {
-    requestPermissions();
-  }, []);
+  const [uri, setUri] = useState(null);
+
   return (
     <GestureHandlerRootView>
       <Screen>
-        <Text>test</Text>
+        <ImageInput imageUri={uri} onChangeImage={(uri) => setUri(uri)} />
       </Screen>
     </GestureHandlerRootView>
   );

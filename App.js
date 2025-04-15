0d1104938840f299a148e05293f86639548c3797
MilitsaTsvetkova <militsa88@yahoo.com>
2025-04-15 09:27:46 +0300

    permissions module/ request permissions ImagePicker

--- App.js
+++ App.js
@@ -1,10 +1,29 @@
+import * as ImagePicker from "expo-image-picker";
+import { useEffect } from "react";
+import { Text } from "react-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
-import ListingEditScreen from "./app/screens/ListingEditScreen";
+import Screen from "./app/components/Screen";
 
 export default function App() {
+  const requestPermissions = async () => {
+    //alternative - can use this to request multiple permissions
+    // const { granted } = await Permissions.askAsync(
+    //   Permissions.MEDIA_LIBRARY,
+    //   Permissions.CAMERA
+    // );
+    const { granted } = await ImagePicker.requestCameraPermissionsAsync();
+    if (!granted) {
+      alert("Sorry, we need camera roll permissions to make this work!");
+    }
+  };
+  useEffect(() => {
+    requestPermissions();
+  }, []);
   return (
     <GestureHandlerRootView>
-      <ListingEditScreen />
+      <Screen>
+        <Text>test</Text>
+      </Screen>
     </GestureHandlerRootView>
   );
 }

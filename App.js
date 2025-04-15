0d507c91dc0a4ca20f76c79df10f7197b6e87aaa
MilitsaTsvetkova <militsa88@yahoo.com>
2025-04-15 10:59:10 +0300

    basic ImageInputList

--- App.js
+++ App.js
@@ -1,15 +1,26 @@
 import { useState } from "react";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
-import ImageInput from "./app/components/ImageInput";
+import ImageInputList from "./app/components/ImageInputList";
 import Screen from "./app/components/Screen";
 
 export default function App() {
-  const [uri, setUri] = useState(null);
+  const [imageUris, setImageUris] = useState([]);
+
+  const handleAdd = (uri) => {
+    setImageUris((prev) => [...prev, uri]);
+  };
+  const handleRemove = (uri) => {
+    setImageUris((prev) => prev.filter((imageUri) => imageUri !== uri));
+  };
 
   return (
     <GestureHandlerRootView>
       <Screen>
-        <ImageInput imageUri={uri} onChangeImage={(uri) => setUri(uri)} />
+        <ImageInputList
+          imageUris={imageUris}
+          onAddImage={handleAdd}
+          onRemoveImage={handleRemove}
+        />
       </Screen>
     </GestureHandlerRootView>
   );

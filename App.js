475230bfc41bf271e43170de5cec3dac5cf43973
MilitsaTsvetkova <militsa88@yahoo.com>
2025-04-15 21:01:59 +0300

    Stack Navigator

--- App.js
+++ App.js
@@ -1,10 +1,26 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import ListingEditScreen from "./app/screens/ListingEditScreen";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
+
+const Stack = createNativeStackNavigator();
+
+const StackNavigator = () => {
+  return (
+    <Stack.Navigator initialRouteName="WelcomeScreen">
+      <Stack.Screen name="WelcomeScreen" component={WelcomeScreen} />
+      <Stack.Screen name="ListingEdit" component={ListingEditScreen} />
+    </Stack.Navigator>
+  );
+};
 
 export default function App() {
   return (
-    <GestureHandlerRootView>
-      <ListingEditScreen />
-    </GestureHandlerRootView>
+    <NavigationContainer>
+      <GestureHandlerRootView>
+        <StackNavigator />
+      </GestureHandlerRootView>
+    </NavigationContainer>
   );
 }

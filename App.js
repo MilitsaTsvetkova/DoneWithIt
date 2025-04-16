a9ac5a0aae67114af3c3a10be94274b9349e61a1
MilitsaTsvetkova <militsa88@yahoo.com>
2025-04-16 09:55:44 +0300

    AppNavigator component

--- App.js
+++ App.js
@@ -1,23 +1,13 @@
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
-import AuthNavigator from "./app/navigation/AuthNavigator";
+import AppNavigator from "./app/navigation/AppNavigator";
 import navigationTheme from "./app/navigation/navigationTheme";
-
-const Tab = createBottomTabNavigator();
-const TabNavigator = () => {
-  return (
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={AuthNavigator} />
-    </Tab.Navigator>
-  );
-};
 
 export default function App() {
   return (
     <NavigationContainer theme={navigationTheme}>
       <GestureHandlerRootView>
-        <TabNavigator />
+        <AppNavigator />
       </GestureHandlerRootView>
     </NavigationContainer>
   );

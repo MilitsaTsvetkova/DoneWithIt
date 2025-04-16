0c8fc4ede2f71ad598bfd4280860a11ddf0890da
MilitsaTsvetkova <militsa88@yahoo.com>
2025-04-16 09:47:28 +0300

    AuthNavigator component

--- App.js
+++ App.js
@@ -1,21 +1,14 @@
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
-import ListingEditScreen from "./app/screens/ListingEditScreen";
-import LoginScreen from "./app/screens/LoginScreen";
-import RegisterScreen from "./app/screens/RegisterScreen";
-import WelcomeScreen from "./app/screens/WelcomeScreen";
+import AuthNavigator from "./app/navigation/AuthNavigator";
 
-const Stack = createNativeStackNavigator();
-
-const StackNavigator = () => {
+const Tab = createBottomTabNavigator();
+const TabNavigator = () => {
   return (
-    <Stack.Navigator initialRouteName="WelcomeScreen">
-      <Stack.Screen name="WelcomeScreen" component={WelcomeScreen} />
-      <Stack.Screen name="ListingEdit" component={ListingEditScreen} />
-      <Stack.Screen name="Login" component={LoginScreen} />
-      <Stack.Screen name="Register" component={RegisterScreen} />
-    </Stack.Navigator>
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={AuthNavigator} />
+    </Tab.Navigator>
   );
 };
 
@@ -23,7 +16,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <GestureHandlerRootView>
-        <StackNavigator />
+        <TabNavigator />
       </GestureHandlerRootView>
     </NavigationContainer>
   );

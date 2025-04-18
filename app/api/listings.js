a2e2fcce8a78a04a0f16b281046028a6594088de
MilitsaTsvetkova <militsa88@yahoo.com>
2025-04-18 20:00:10 +0300

    extend api listing with addListing

--- app/api/listings.js
+++ app/api/listings.js
@@ -4,6 +4,30 @@ const endpoint = "/listings";
 
 export const getListings = () => client.get(endpoint);
 
+export const addListing = (listing) => {
+  const data = new FormData();
+
+  data.append("title", listing.title);
+  data.append("price", listing.price);
+  data.append("description", listing.description);
+  data.append("categoryId", listing.category.value);
+
+  listing.images.forEach((image, index) => {
+    data.append("images", {
+      name: "image" + index,
+      type: "image/jpeg",
+      uri: image,
+    });
+  });
+
+  if (listing.location) {
+    data.append("location", JSON.stringify(listing.location));
+  }
+
+  return client.post(endpoint, data);
+};
+
 export default {
   getListings,
+  addListing,
 };
